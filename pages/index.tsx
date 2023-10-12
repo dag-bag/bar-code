dba0b038385d945daa80172c02585995fdd1e430
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-10-12 17:58:23 +0530

    git init

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import QRCode from "react-qr-code";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen"></div>
+  );
+}
